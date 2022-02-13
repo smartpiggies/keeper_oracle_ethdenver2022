c351777c70cc5b7b72bf6720032b34b0a9b03256
tobyjaguar <talgya@gmail.com>
2022-02-13 12:11:53 -0700

    update remix paste, working ETHUSD spot price

--- contracts/smartpiggies/ResolverKeeper.sol
+++ contracts/smartpiggies/ResolverKeeper.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 /**
  *  Submission for EthDenver 20222 Live hacking participation Event
  *  Implement a chainlink keeper design pattern into the SmartPiggies
@@ -8,60 +7,170 @@
  *  register a future price callback, where by a keeper job
  *  will return a price of the underlying at time of expiry
  */
-pragma solidity ^0.8.7;
+ // SPDX-License-Identifier: MIT
+ pragma solidity ^0.8.7;
 
-contract ResolverKeeper {
+ contract ResolverKeeper {
+     address owner;
+     address SmartPiggies;
+     address OracleKeeper;
 
-    address public owner;
-    address SmartPiggies;
-    address OracleKeeper;
+     bytes32 constant TX_SUCCESS = bytes32(0x0000000000000000000000000000000000000000000000000000000000000001);
+     uint256 public resolution;
+     bytes32 public requestedId;
+     uint256 public returnedData;
 
-    constructor() {
-        owner = msg.sender;
-    }
+     struct Request {
+         address requester;
+         address funder;
+         uint256 tokenId;
+         uint8 requestType;
+     }
 
-    struct Request {
-        address requester;
-        address funder;
-        uint256 tokenId;
-        uint8 requestType;
-    }
+     mapping(bytes32 => Request) public requests;
 
-    constructor(address _SmartPiggies, address _okeeper)
-        public
-    {
-        SmartPiggies = _SmartPiggies;
-        OracleKeeper = _okeeper;
-        owner = msg.sender;
-    }
+     constructor(address _SmartPiggies, address _OracleKeeper, uint256 _resolution)
+     {
+         SmartPiggies = _SmartPiggies;
+         OracleKeeper = _OracleKeeper;
+         resolution = _resolution;
+         owner = msg.sender;
+     }
 
-    function register()
-     public
-     returns (bool)
-    {
-      require(msg.sender == SmartPiggies, "Caller is not SmartPiggies.");
-      return true;
-    }
+     modifier onlyOwner()
+     {
+         require(msg.sender == owner, "Caller is not Owner.");
+         _;
+     }
 
-    function fetchData()
+     modifier onlyPiggies()
+     {
+         require(msg.sender == SmartPiggies, "Caller is not SmartPiggies.");
+         _;
+     }
+
+     function register
+     (
+         address _funder,
+         uint256 _oracleFee,
+         uint256 _tokenId,
+         uint256 _expiry,
+         uint8 _requestType
+     )
       public
+      onlyPiggies
       returns (bool)
-    {
-        require(msg.sender == SmartPiggies, "Caller is not SmartPiggies.");
-        return true;
-    }
+     {
+         _oracleFee; // throw away value, maintains method signature for SmartPiggies
 
-    function getPriceCallback()
-      public
-      returns (bool)
-    {
-      return true;
-    }
+         // generate a unique request id
+         bytes32 requestId = keccak256(abi.encodePacked(_funder,_tokenId,_expiry));
 
-    function kill()
-        public
-    {
-        require(msg.sender == owner);
-        selfdestruct(payable(owner));
-    }
-}
+         bytes memory payload = abi.encodeWithSignature(
+             "registerDataCall(bytes32,uint256,bytes4)",
+             requestId,
+             _expiry,
+             this.getPriceCallback.selector
+         );
+
+         (bool registered, bytes memory result) = address(OracleKeeper).call(payload);
+         bytes32 txCheck = abi.decode(result, (bytes32));
+         require(registered && txCheck == TX_SUCCESS, "register data call failed.");
+
+         requests[requestId] = Request({
+           requester: msg.sender,
+           funder: _funder,
+           tokenId: _tokenId,
+           requestType: _requestType
+           });
+
+         return true;
+     }
+
+     function fetchData(
+         address _funder,
+         uint256 _oracleFee,
+         uint256 _tokenId,
+         uint256 _expiry,
+         uint8 _requestType
+     )
+         public
+         onlyPiggies
+         returns (bool)
+     {
+         _oracleFee; // throw away value, maintains method signature for SmartPiggies
+         _requestType;
+
+         // generate a unique request id
+         bytes32 requestId = keccak256(abi.encodePacked(_funder,_tokenId,_expiry));
+
+         bytes memory payload = abi.encodeWithSignature(
+             "fulfillDataCall(bytes32,uint256,bytes4)",
+             requestId,
+             _expiry,
+             this.getPriceCallback.selector
+         );
+
+         (bool registered, bytes memory result) = address(OracleKeeper).call(payload);
+         bytes32 txCheck = abi.decode(result, (bytes32));
+         require(registered && txCheck == TX_SUCCESS, "register data call failed.");
+         //(bool success, ) = address(OracleKeeper).call(payload);
+         //success;
+         return true;
+     }
+
+     function getPriceCallback(bytes32 _requestId, uint256 _data, bytes memory _signature)
+         public
+         returns (bool)
+     {
+         // maintain signature compatibility
+         _signature;
+         require(msg.sender == OracleKeeper, "Caller is not the oracle keeper");
+
+         requestedId = _requestId;
+         returnedData = _data;
+
+         bytes memory payload = abi.encodeWithSignature(
+             "callback(uint256,uint256,uint8)",
+             requests[_requestId].tokenId,
+             (_data / resolution),
+             requests[_requestId].requestType
+         );
+         // SmartPiggies callback does not return
+         (bool success, ) = address(requests[_requestId].requester).call(payload);
+         success; // send this tx regardless
+         return true;
+     }
+
+     function updateResolution(uint256 _newResolution)
+         public
+         onlyOwner
+         returns (bool)
+     {
+         resolution = _newResolution;
+         return true;
+     }
+
+     function getSmartPiggies()
+         public
+         view
+         returns (address)
+     {
+         return SmartPiggies;
+     }
+
+     function getOracle()
+         public
+         view
+         returns (address)
+     {
+         return OracleKeeper;
+     }
+
+     function kill()
+         public
+         onlyOwner
+     {
+         selfdestruct(payable(owner));
+     }
+ } 
