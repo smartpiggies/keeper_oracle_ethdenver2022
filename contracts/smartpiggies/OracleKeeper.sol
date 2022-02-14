f688cf40d1529b92c3c151580cc9b27fbbd8f590
tobyjaguar <talgya@gmail.com>
2022-02-14 00:28:28 -0700

    it works, but it doesnt

--- contracts/smartpiggies/OracleKeeper.sol
+++ contracts/smartpiggies/OracleKeeper.sol
@@ -9,213 +9,255 @@
  *  will be forwarded to the performUpkeep phase to retrieve a price
  *  and return the price back to the SmartPiggies contract.
  */
- // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 //import "@chainlink/contracts/src/v0.7/KeeperCompatible.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Owned {
-    address public owner;
-    constructor(address _owner)
-    {
-        owner = _owner;
-    }
+   address public owner;
+   constructor(address _owner)
+   {
+       owner = _owner;
+   }
 
-    modifier onlyOwner()
-    {
-        require(msg.sender == owner);
-        _;
-    }
+   modifier onlyOwner()
+   {
+       require(msg.sender == owner);
+       _;
+   }
 }
 
 /**
- *  Chainlink price addresses
- *  ETHUSD 0x0715A7794a1dc8e42615F059dD6e406A6594651A
- *  BTCUSD 0x007A22900a3B98143368Bd5906f8E17e9867581b
- *  MATICUSD 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
- */
+*  Chainlink price addresses
+*  ETHUSD 0x0715A7794a1dc8e42615F059dD6e406A6594651A
+*  BTCUSD 0x007A22900a3B98143368Bd5906f8E17e9867581b
+*  MATICUSD 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
+*/
 abstract contract UsingLookUp is Owned {
-    mapping(string => address) public lookups;
+   mapping(string => address) public lookups;
 
-    function setLookup(string memory _asset, address _address)
-        public
-        onlyOwner
-        returns (bool)
-    {
-        lookups[_asset] = _address;
-        return true;
-    }
+   function setLookup(string memory _asset, address _address)
+       public
+       onlyOwner
+       returns (bool)
+   {
+       lookups[_asset] = _address;
+       return true;
+   }
 
 }
 
 contract OracleKeeper is UsingLookUp {
-    bytes32 constant TX_SUCCESS = bytes32(0x0000000000000000000000000000000000000000000000000000000000000001);
-    uint256 constant capacity = 200;
+   bytes32 constant TX_SUCCESS = bytes32(0x0000000000000000000000000000000000000000000000000000000000000001);
+   uint256 constant public capacity = 200;
 
-    uint256 head = 0;
-    uint256 tail = 0;
-    address public lastAsset;
-    struct Request {
-        address requester;
-        bytes32 requestId;
-        uint256 expiry;
-        bytes4 callback;
-    }
+   uint256 public head = 0;
+   uint256 public tail = 0;
+   bool public bufferFull = false;
+   address public requestedAddress;
 
-    struct Job {
-        bytes32 requestId;
-        uint256 expiry;
-    }
+   struct Request {
+       address requester;
+       bytes32 requestId;
+       uint256 expiry;
+       bytes4 callback;
+   }
 
-    mapping(bytes32 => Request) public requests;
-    mapping(address => string) public assets;
-    mapping(address => bool) public resolvers;
+   struct Job {
+       bytes32 requestId;
+       uint256 expiry;
+   }
 
-    Job[capacity] jobs;
+   mapping(bytes32 => Request) public requests;
+   mapping(address => string) public assets;
+   mapping(address => bool) public resolvers;
 
-    constructor()
-        Owned(msg.sender)
-    {
+   Job[capacity] public jobs;
 
-    }
+   constructor()
+       Owned(msg.sender)
+   { }
 
-    modifier onlyKnown()
-    {
-        // only known resolvers
-        require(resolvers[msg.sender], "resolver is not recognized");
-        _;
-    }
+   modifier onlyKnown()
+   {
+       // only known resolvers
+       require(resolvers[msg.sender], "resolver is not recognized");
+       _;
+   }
 
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice(string memory _asset) public view returns (int) {
-        AggregatorV3Interface priceFeed;
-        priceFeed = AggregatorV3Interface(lookups[_asset]);
-        (
-            uint80 roundID,
-            int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        roundID;
-        price;
-        startedAt;
-        timeStamp;
-        answeredInRound;
-        return price;
-    }
+   /**
+    * Returns the latest price
+    */
+   function getLatestPrice(string memory _asset) public view returns (int) {
+       AggregatorV3Interface priceFeed;
+       priceFeed = AggregatorV3Interface(lookups[_asset]);
+       (
+           uint80 roundID,
+           int price,
+           uint startedAt,
+           uint timeStamp,
+           uint80 answeredInRound
+       ) = priceFeed.latestRoundData();
+       roundID;
+       price;
+       startedAt;
+       timeStamp;
+       answeredInRound;
+       return price;
+   }
 
-    function checkUpkeep(bytes calldata /* checkData */)
-        external
-        returns (bool upkeepNeeded, bytes memory /* performData */)
-    {
-        // performed off-chain to determine if performUpkeep is to be executed
-        // We don't use the checkData in this example.
-        // The checkData is defined when the Upkeep was registered.
-    }
+   function checkUpkeep(bytes calldata /* checkData */)
+       external
+       view
+       returns (bool upkeepNeeded, bytes memory performData)
+   {
+       // performed off-chain to determine if performUpkeep is to be executed
+       // We don't use the checkData in this example.
+       // The checkData is defined when the Upkeep was registered.
+       uint256 expiry;
+       upkeepNeeded = false;
+       for(uint256 i = 0; i < capacity; i++)
+       {
+           expiry = jobs[i+tail%capacity].expiry;
+           if(expiry != 0 && expiry < block.timestamp)
+           {
+               upkeepNeeded = true;
+               break;
+           }
+           if(i+tail%capacity == head) // if the index meets the head, bail
+           {
+               break;
+           }
+       }
+       performData = (upkeepNeeded) ? abi.encode(0x01) : abi.encode(0x00);
+       return (upkeepNeeded, performData);
+   }
 
-    function performUpkeep(bytes calldata /* performData */) external {
-        // We highly recommend revalidating the upkeep in the performUpkeep function
-        // We don't use the performData in this example.
-        // The performData is generated by the Keeper's call to your checkUpkeep function
-    }
+   function performUpkeep(bytes calldata /* performData */) external {
+       // We highly recommend revalidating the upkeep in the performUpkeep function
+       // We don't use the performData in this example.
+       // The performData is generated by the Keeper's call to your checkUpkeep function
+       bytes32 id;
+       address requester;
+       uint256 expiry;
+       for(uint256 i = 0; i < capacity; i++)
+       {
+           expiry = jobs[i+tail%capacity].expiry;
+           if(expiry != 0 && expiry < block.timestamp)
+           {
+               id = jobs[tail].requestId;
+               requester = requests[id].requester;
+               _processDataCall(id, assets[requester]);
+           }
+           if(i+tail%capacity == head) // if the index meets the head, bail
+           {
+               break;
+           }
+       }
+   }
 
-    function registerDataCall(bytes32 _requestId, uint256 _expiry, bytes4 _callback)
-        public
-        onlyKnown
-        returns (bool)
-    {
+   function registerDataCall(bytes32 _requestId, uint256 _expiry, bytes4 _callback)
+       public
+       onlyKnown
+       returns (bool)
+   {
+       require(bufferFull == false, "Jobs buffer full");
 
-        // record requestId
-        requests[_requestId] = Request ({
-            requester: msg.sender,
-            requestId: _requestId,
-            expiry: _expiry,
-            callback: _callback
-        });
+       // record requestId
+       requests[_requestId] = Request ({
+           requester: msg.sender,
+           requestId: _requestId,
+           expiry: _expiry,
+           callback: _callback
+       });
 
-        // increase head
-        head = head + 1 % capacity;
+       // update jobs buffer
+       jobs[head] = Job ({
+           requestId: _requestId,
+           expiry: _expiry
+       });
 
-        // update jobs buffer
-        jobs[head] = Job ({
-            requestId: _requestId,
-            expiry: _expiry
-        });
+       // increase head
+       head = head + 1 % capacity;
 
-        return true;
-    }
+       bufferFull = (head == tail) ? true : false;
+       return true;
+   }
 
-    function fulfillDataCall(bytes32 _requestId, uint256 _expiry, bytes4 _callback)
-        public
-        onlyKnown
-        returns (bool)
-    {
-        // for signature continuity
-        _expiry;
-        _callback;
-        // retrieve request
-        Request storage r = requests[_requestId];
-        int256 price = getLatestPrice(assets[msg.sender]);
-        bytes memory signature = "signature";
-        bytes memory payload = abi.encodeWithSelector(r.callback, _requestId, uint256(price), signature);
-        (bool success, bytes memory result) = address(r.requester).call(payload);
-        bytes32 txCheck = abi.decode(result, (bytes32));
-        require(success && txCheck == TX_SUCCESS, "callback to resolver failed");
+   function fulfillDataCall(bytes32 _requestId, uint256 _expiry, bytes4 _callback)
+       public
+       onlyKnown
+       returns (bool)
+   {
+       // for signature continuity
+       _expiry;
+       _callback;
+       _processDataCall(_requestId, assets[msg.sender]);
+       return true;
+   }
 
-        // clean up jobs buffer
-        uint256 index = 0;
-        for(uint256 i = 0; i < capacity; i++)
-        {
-            if(jobs[i+tail%capacity].requestId == _requestId)
-            {
-                index = i;
-                break;
-            }
-        }
-        // when index of removal is located
-        // clean up the buffer
-        for(uint256 i = 0; i < capacity; i++)
-        {
-            if(index == tail)
-            {
-                delete jobs[index];
-                tail = (tail + 1) % capacity;
-                break;
-            }
-            if(index == 0)
-            {
-                jobs[index] = jobs[capacity-1];
-                index = capacity - 1;
-            }
-            else
-            {
-                jobs[index] = jobs[index-1];
-                index = index - 1;
-            }
+   function _processDataCall(bytes32 _requestId, string memory _asset)
+       internal
+   {
+       // retrieve request
+       Request storage r = requests[_requestId];
+       requestedAddress = lookups[_asset];
+       int256 price = getLatestPrice(_asset);
+       bytes memory signature = "signature";
+       bytes memory payload = abi.encodeWithSelector(r.callback, _requestId, uint256(price), signature);
+       (bool success, bytes memory result) = address(r.requester).call(payload);
+       bytes32 txCheck = abi.decode(result, (bytes32));
+       require(success && txCheck == TX_SUCCESS, "callback to resolver failed");
 
-        }
+       // clean up jobs buffer
+       uint256 index = 0;
+       for(uint256 i = 0; i < capacity; i++)
+       {
+           if(jobs[i+tail%capacity].requestId == _requestId)
+           {
+               index = i;
+               break;
+           }
+       }
+       // when index of removal is located
+       // clean up the buffer
+       for(uint256 i = 0; i < capacity; i++)
+       {
+           if(index == tail)
+           {
+               delete jobs[index];
+               tail = (tail + 1) % capacity;
+               break;
+           }
+           if(index == 0)
+           {
+               jobs[index] = jobs[capacity-1];
+               index = capacity - 1;
+           }
+           else
+           {
+               jobs[index] = jobs[index-1];
+               index = index - 1;
+           }
+       }
+   }
 
-        return true;
-    }
+   function updateResolver(address _resolver, string memory _asset)
+       public
+       onlyOwner
+       returns (bool)
+   {
+       assets[_resolver] = _asset;
+       if(!resolvers[_resolver]) resolvers[_resolver] = true;
+       return true;
+   }
 
-    function updateResolver(address _resolver, string memory _asset)
-        public
-        onlyOwner
-        returns (bool)
-    {
-        assets[_resolver] = _asset;
-        if(!resolvers[_resolver]) resolvers[_resolver] = true;
-        return true;
-    }
-
-    function kill()
-        public
-        onlyOwner
-    {
-        selfdestruct(payable(owner));
-    }
+   function kill()
+       public
+       onlyOwner
+   {
+       selfdestruct(payable(owner));
+   }
 }
--- contracts/smartpiggies/ResolverKeeper.sol
+++ contracts/smartpiggies/ResolverKeeper.sol
@@ -7,170 +7,170 @@
  *  register a future price callback, where by a keeper job
  *  will return a price of the underlying at time of expiry
  */
- // SPDX-License-Identifier: MIT
- pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
- contract ResolverKeeper {
-     address owner;
-     address SmartPiggies;
-     address OracleKeeper;
+contract ResolverKeeper {
+  address owner;
+  address SmartPiggies;
+  address OracleKeeper;
 
-     bytes32 constant TX_SUCCESS = bytes32(0x0000000000000000000000000000000000000000000000000000000000000001);
-     uint256 public resolution;
-     bytes32 public requestedId;
-     uint256 public returnedData;
+  bytes32 constant TX_SUCCESS = bytes32(0x0000000000000000000000000000000000000000000000000000000000000001);
+  uint256 public resolution;
+  bytes32 public requestedId;
+  uint256 public returnedData;
 
-     struct Request {
-         address requester;
-         address funder;
-         uint256 tokenId;
-         uint8 requestType;
-     }
+  struct Request {
+      address requester;
+      address funder;
+      uint256 tokenId;
+      uint8 requestType;
+  }
 
-     mapping(bytes32 => Request) public requests;
+  mapping(bytes32 => Request) public requests;
 
-     constructor(address _SmartPiggies, address _OracleKeeper, uint256 _resolution)
-     {
-         SmartPiggies = _SmartPiggies;
-         OracleKeeper = _OracleKeeper;
-         resolution = _resolution;
-         owner = msg.sender;
-     }
+  constructor(address _SmartPiggies, address _OracleKeeper, uint256 _resolution)
+  {
+      SmartPiggies = _SmartPiggies;
+      OracleKeeper = _OracleKeeper;
+      resolution = _resolution;
+      owner = msg.sender;
+  }
 
-     modifier onlyOwner()
-     {
-         require(msg.sender == owner, "Caller is not Owner.");
-         _;
-     }
+  modifier onlyOwner()
+  {
+      require(msg.sender == owner, "Caller is not Owner.");
+      _;
+  }
 
-     modifier onlyPiggies()
-     {
-         require(msg.sender == SmartPiggies, "Caller is not SmartPiggies.");
-         _;
-     }
+  modifier onlyPiggies()
+  {
+      require(msg.sender == SmartPiggies, "Caller is not SmartPiggies.");
+      _;
+  }
 
-     function register
-     (
-         address _funder,
-         uint256 _oracleFee,
-         uint256 _tokenId,
-         uint256 _expiry,
-         uint8 _requestType
-     )
+  function register
+  (
+      address _funder,
+      uint256 _oracleFee,
+      uint256 _tokenId,
+      uint256 _expiry,
+      uint8 _requestType
+  )
+   public
+   onlyPiggies
+   returns (bool)
+  {
+      _oracleFee; // throw away value, maintains method signature for SmartPiggies
+
+      // generate a unique request id
+      bytes32 requestId = keccak256(abi.encodePacked(_funder,_tokenId,_expiry));
+
+      bytes memory payload = abi.encodeWithSignature(
+          "registerDataCall(bytes32,uint256,bytes4)",
+          requestId,
+          _expiry,
+          this.getPriceCallback.selector
+      );
+
+      (bool registered, bytes memory result) = address(OracleKeeper).call(payload);
+      bytes32 txCheck = abi.decode(result, (bytes32));
+      require(registered && txCheck == TX_SUCCESS, "register data call failed.");
+
+      requests[requestId] = Request({
+        requester: msg.sender,
+        funder: _funder,
+        tokenId: _tokenId,
+        requestType: _requestType
+        });
+
+      return true;
+  }
+
+  function fetchData(
+      address _funder,
+      uint256 _oracleFee,
+      uint256 _tokenId,
+      uint256 _expiry,
+      uint8 _requestType
+  )
       public
       onlyPiggies
       returns (bool)
-     {
-         _oracleFee; // throw away value, maintains method signature for SmartPiggies
+  {
+      _oracleFee; // throw away value, maintains method signature for SmartPiggies
+      _requestType;
 
-         // generate a unique request id
-         bytes32 requestId = keccak256(abi.encodePacked(_funder,_tokenId,_expiry));
+      // generate a unique request id
+      bytes32 requestId = keccak256(abi.encodePacked(_funder,_tokenId,_expiry));
 
-         bytes memory payload = abi.encodeWithSignature(
-             "registerDataCall(bytes32,uint256,bytes4)",
-             requestId,
-             _expiry,
-             this.getPriceCallback.selector
-         );
+      bytes memory payload = abi.encodeWithSignature(
+          "fulfillDataCall(bytes32,uint256,bytes4)",
+          requestId,
+          _expiry,
+          this.getPriceCallback.selector
+      );
 
-         (bool registered, bytes memory result) = address(OracleKeeper).call(payload);
-         bytes32 txCheck = abi.decode(result, (bytes32));
-         require(registered && txCheck == TX_SUCCESS, "register data call failed.");
+      (bool registered, bytes memory result) = address(OracleKeeper).call(payload);
+      bytes32 txCheck = abi.decode(result, (bytes32));
+      require(registered && txCheck == TX_SUCCESS, "register data call failed.");
+      //(bool success, ) = address(OracleKeeper).call(payload);
+      //success;
+      return true;
+  }
 
-         requests[requestId] = Request({
-           requester: msg.sender,
-           funder: _funder,
-           tokenId: _tokenId,
-           requestType: _requestType
-           });
+  function getPriceCallback(bytes32 _requestId, uint256 _data, bytes memory _signature)
+      public
+      returns (bool)
+  {
+      // maintain signature compatibility
+      _signature;
+      require(msg.sender == OracleKeeper, "Caller is not the oracle keeper");
 
-         return true;
-     }
+      requestedId = _requestId;
+      returnedData = _data;
 
-     function fetchData(
-         address _funder,
-         uint256 _oracleFee,
-         uint256 _tokenId,
-         uint256 _expiry,
-         uint8 _requestType
-     )
-         public
-         onlyPiggies
-         returns (bool)
-     {
-         _oracleFee; // throw away value, maintains method signature for SmartPiggies
-         _requestType;
+      bytes memory payload = abi.encodeWithSignature(
+          "callback(uint256,uint256,uint8)",
+          requests[_requestId].tokenId,
+          (_data / resolution),
+          requests[_requestId].requestType
+      );
+      // SmartPiggies callback does not return
+      (bool success, ) = address(requests[_requestId].requester).call(payload);
+      success; // send this tx regardless
+      return true;
+  }
 
-         // generate a unique request id
-         bytes32 requestId = keccak256(abi.encodePacked(_funder,_tokenId,_expiry));
+  function updateResolution(uint256 _newResolution)
+      public
+      onlyOwner
+      returns (bool)
+  {
+      resolution = _newResolution;
+      return true;
+  }
 
-         bytes memory payload = abi.encodeWithSignature(
-             "fulfillDataCall(bytes32,uint256,bytes4)",
-             requestId,
-             _expiry,
-             this.getPriceCallback.selector
-         );
+  function getSmartPiggies()
+      public
+      view
+      returns (address)
+  {
+      return SmartPiggies;
+  }
 
-         (bool registered, bytes memory result) = address(OracleKeeper).call(payload);
-         bytes32 txCheck = abi.decode(result, (bytes32));
-         require(registered && txCheck == TX_SUCCESS, "register data call failed.");
-         //(bool success, ) = address(OracleKeeper).call(payload);
-         //success;
-         return true;
-     }
+  function getOracle()
+      public
+      view
+      returns (address)
+  {
+      return OracleKeeper;
+  }
 
-     function getPriceCallback(bytes32 _requestId, uint256 _data, bytes memory _signature)
-         public
-         returns (bool)
-     {
-         // maintain signature compatibility
-         _signature;
-         require(msg.sender == OracleKeeper, "Caller is not the oracle keeper");
-
-         requestedId = _requestId;
-         returnedData = _data;
-
-         bytes memory payload = abi.encodeWithSignature(
-             "callback(uint256,uint256,uint8)",
-             requests[_requestId].tokenId,
-             (_data / resolution),
-             requests[_requestId].requestType
-         );
-         // SmartPiggies callback does not return
-         (bool success, ) = address(requests[_requestId].requester).call(payload);
-         success; // send this tx regardless
-         return true;
-     }
-
-     function updateResolution(uint256 _newResolution)
-         public
-         onlyOwner
-         returns (bool)
-     {
-         resolution = _newResolution;
-         return true;
-     }
-
-     function getSmartPiggies()
-         public
-         view
-         returns (address)
-     {
-         return SmartPiggies;
-     }
-
-     function getOracle()
-         public
-         view
-         returns (address)
-     {
-         return OracleKeeper;
-     }
-
-     function kill()
-         public
-         onlyOwner
-     {
-         selfdestruct(payable(owner));
-     }
- }
+  function kill()
+      public
+      onlyOwner
+  {
+      selfdestruct(payable(owner));
+  }
+}
