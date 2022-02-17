97230c5c1a61118e6bf406f9af11e2673370fa2b
tobyjaguar <talgya@gmail.com>
2022-02-17 11:18:02 -0700

    update frontend

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,8 @@ import {
   Container,
   Grid,
   Header,
-  Input } from 'semantic-ui-react';
+  Input,
+  Segment } from 'semantic-ui-react';
 
 import abi from './contracts/abi/ResolverKeeper.json';
 import bytecode from './contracts/bytecode/ResolverKeeper.json';
@@ -27,11 +28,27 @@ init();
 
 const  App = () => {
   const [inputAsset, setInputAsset] = useState('');
+  const [resolver, setResolver] = useState('');
+  const [onChainAddress, setonChainAddress] = useState('');
+  const [resolverAddress, setResolverAddress] = useState('');
 
   const deploy = async () => {
     factory = new ethers.ContractFactory(abi, bytecode, signer);
-    contract = await factory.deploy();
-    console.log(contract.address);
+    //contract = await factory.deploy();
+    setResolver(await factory.deploy());
+    setResolverAddress(resolver.address);
+    //console.log(contract.address);
+    console.log(resolver.address);
+  }
+
+  const register = async () => {
+
+    console.log();
+  }
+
+  const setLookup = async () => {
+    
+    console.log();
   }
 
   return (
@@ -39,10 +56,24 @@ const  App = () => {
       <Header as='h1'>Oracle Creator</Header>
       <Grid columns={3} stackable>
         <Grid.Column>
-          <Header as='h3'>Deploy</Header>
+          <Header as='h3'>Deploy Resolver</Header>
+
+          <Button
+            onClick={() => deploy()}
+          >
+            Deploy
+          </Button>
+
+          <Segment>
+          {(resolverAddress == '') ?
+            <Header as='h3'>{resolverAddress}</Header> :
+            null}
+          </Segment>
+
+          <br></br>
+          <Header as='h3'>Register Resolver</Header>
 
           <Input
-            size='large'
             placeholder='Asset...'
             value={inputAsset}
             onChange={e => setInputAsset(e.target.value)}
@@ -51,8 +82,31 @@ const  App = () => {
           <Button
             onClick={() => deploy()}
           >
-            Deploy
+            Register
           </Button>
+
+          <br></br>
+          <br></br>
+          <Header as='h3'>Set Reverse Lookup</Header>
+
+          <Input
+            placeholder='Asset...'
+            value={inputAsset}
+            onChange={e => setInputAsset(e.target.value)}
+          />
+          <br></br>
+          <Input
+            placeholder='Chainlink Address'
+            value={onChainAddress}
+            onChange={e => setonChainAddress(e.target.value)}
+          />
+          <br></br>
+          <Button
+            onClick={() => deploy()}
+          >
+            Set
+          </Button>
+
         </Grid.Column>
       </Grid>
     </Container>
